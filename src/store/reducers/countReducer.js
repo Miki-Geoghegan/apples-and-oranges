23f2be7d69abeafdda5671b59a13d239f2a3d500
Miki-Geoghegan <mikigeoghegan@gmail.com>
2021-12-29 16:27:17 +0100

    creating count constants

--- src/store/reducers/countReducer.js
+++ src/store/reducers/countReducer.js
@@ -1,15 +1,17 @@
+import { DECREASE_COUNT, INCREASE_COUNT } from '../constants/count'
+
 const initialCountState = {
   count: 0
 }
 
 const countReducer = (state = initialCountState, action) => {
   switch (action.type) {
-  case 'INCREASE-COUNT':
+  case INCREASE_COUNT:
     return {
       ...state,
       count: state.count + 1
     }
-  case 'DECREASE-COUNT':
+  case DECREASE_COUNT:
     return {
       ...state,
       count: state.count - 1
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const APPLE = 'apple'
 export const ORANGE = 'orange'
-export const INCREASE_COUNT = 'increase_count'
-export const DECREASE_COUNT = 'decrease_count'
+export const INCREASE_COUNT = 'INCREASE-COUNT'
+export const DECREASE_COUNT = 'DECREASE-COUNT'
 
